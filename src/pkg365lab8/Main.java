f4dc2408caf7ae656e2e4bc2eb0e4ed5f592c01e
Matt-Bryan <mbryan_379@yahoo.com>
2017-06-08 19:20:39 -0700

    Initial Commit

---
+++ src/pkg365lab8/Main.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg365lab8;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author Matthew
+ */
+public class Main
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        Connection conn = null;
+        String url = "jdbc:mysql://cslvm74.csc.calpoly.edu/students";
+        String user = "mpbryan";
+        String password = "JP3E2Ca9";
+        String statement = "select * from list";
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+        }
+        catch (Exception ex)
+        {
+            System.out.println("Could not open connection");
+        }
+    }
+}
