8881b0c1f8fbb0c016de91dc7634a5dae307e42d
Avinash Sharma <aviator1994@gmail.com>
2017-06-12 16:16:14 -0700

    Finished Q3 using UNION

--- commands.sql
+++ commands.sql
@@ -14,35 +14,35 @@ where Day = '2016-12-30';
 
 -- 2015 - 2016 Increases
 select COUNT(*) as NumIncreases
-from (select IF(SixClose > FifClose,"Increase", "Decrease") as IncDec
-from (select Ticker, AP1.Close as FifClose
-from AdjustedPrices AP1
-where Day = '2015-12-31'
-group by Ticker
-) Fifteen NATURAL JOIN
-(select Ticker, AP2.Close as SixClose
-from AdjustedPrices AP2
-where Day = '2016-12-30'
-group by Ticker
-) Sixteen
-) Changes
-where IncDec = "Increase";
+from (select Sixteen.Ticker, IF(Sixteen.SixClose > Fifteen.FifClose, 'Increase', 'Decrease') as IncDec
+from (select AP.Ticker, AP.Close as FifClose
+from AdjustedPrices AP
+where AP.Day = (select MAX(AP.Day) AS Max
+from AdjustedPrices AP
+where YEAR(AP.Day) = 2015)) Fifteen, 
+(select AP.Ticker, AP.Close as SixClose
+from AdjustedPrices AP
+where AP.Day = (select MAX(AP.Day) AS Max
+from AdjustedPrices AP
+where YEAR(AP.Day) = 2016)) Sixteen
+where Fifteen.Ticker = Sixteen.Ticker) Changes
+where Changes.IncDec = 'Increase';
 
 -- 2015 - 2016 Decreases
 select COUNT(*) as NumDecreases
-from (select IF(SixClose < FifClose,"Decrease", "Increase") as IncDec
-from (select Ticker, AP1.Close as FifClose
-from AdjustedPrices AP1
-where Day = '2015-12-31'
-group by Ticker
-) Fifteen NATURAL JOIN
-(select Ticker, AP2.Close as SixClose
-from AdjustedPrices AP2
-where Day = '2016-12-30'
-group by Ticker
-) Sixteen
-) Changes
-where IncDec = "Decrease";
+from (select Sixteen.Ticker, IF(Sixteen.SixClose < Fifteen.FifClose, 'Decrease', 'Increase') as IncDec
+from (select AP.Ticker, AP.Close as FifClose
+from AdjustedPrices AP
+where AP.Day = (select MAX(AP.Day) AS Max
+from AdjustedPrices AP
+where YEAR(AP.Day) = 2015)) Fifteen, 
+(select AP.Ticker, AP.Close as SixClose
+from AdjustedPrices AP
+where AP.Day = (select MAX(AP.Day) AS Max
+from AdjustedPrices AP
+where YEAR(AP.Day) = 2016)) Sixteen
+where Fifteen.Ticker = Sixteen.Ticker) Changes
+where Changes.IncDec = 'Decrease';
 
 -- Q2 - Report top 10 stocks most heavily traded in 2016.
 select distinct S.Name, AP.Ticker
@@ -53,25 +53,176 @@ order by AP.Volume desc
 limit 10;
 
 -- Q3 - For each year, report the top 5 highest performing stocks in terms
--- of the absolute price increase and top 5 higheest performing stocks in
+-- of the absolute price increase and top 5 highest performing stocks in
 -- terms of relative price increase.
-select LastDays.Day, LastDays.Ticker, (LastDays.Close - FirstDays.Open) as AbsolutePrice
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
 from (select AP.Day, AP.Ticker, AP.Open
 from AdjustedPrices AP
-group by YEAR(AP.Day), AP.Ticker
-) FirstDays,
+where AP.Day = '2010-01-04') FirstDay,
 (select AP.Day, AP.Ticker, AP.Close
-from (select AP.Ticker, MAX(AP.Day) as MaxDate
 from AdjustedPrices AP
-group by YEAR(AP.Day), AP.Ticker
-) Maxes, AdjustedPrices AP
-where Maxes.MaxDate = AP.Day
-and AP.Ticker = Maxes.Ticker
-) LastDays
-where FirstDays.Ticker = LastDays.Ticker
-and YEAR(FirstDays.Day) = YEAR(LastDays.Day)
-group by YEAR(LastDays.Day), LastDays.Ticker
-order by YEAR(LastDays.Day), AbsolutePrice desc;
+where AP.Day = '2010-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y1
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2011-01-03') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2011-12-30') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y2
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2012-01-03') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2012-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y3
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2013-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2013-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y4
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2014-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2014-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y5
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2015-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2015-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y6
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) as AbsolutePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2016-01-04') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2016-12-30') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by AbsolutePrice desc
+limit 5) as Y7;
+
+-- Relative price increases 
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2010-01-04') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2010-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y1
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2011-01-03') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2011-12-30') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y2
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2012-01-03') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2012-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y3
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2013-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2013-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y4
+UNION
+select * 
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2014-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2014-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y5
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2015-01-02') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2015-12-31') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y6
+UNION
+select *
+from (select YEAR(LastDay.Day) AS Year, LastDay.Ticker, (LastDay.Close - FirstDay.Open) / FirstDay.Open as RelativePrice
+from (select AP.Day, AP.Ticker, AP.Open
+from AdjustedPrices AP
+where AP.Day = '2016-01-04') FirstDay,
+(select AP.Day, AP.Ticker, AP.Close
+from AdjustedPrices AP
+where AP.Day = '2016-12-30') LastDay
+where FirstDay.Ticker = LastDay.Ticker
+order by RelativePrice desc
+limit 5) as Y7;
 
 
 -- Q4 - Select 10 stocks to watch in 2017 based on stock market performance
@@ -84,7 +235,6 @@ and YEAR(AP.Day) = 2016
 group by AP.Ticker) Averages
 order by Averages.Avg desc
 limit 10;
-
 
 -- Q5 - 
 
