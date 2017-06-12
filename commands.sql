c3e5d9a42540d2fcac3b3c45267c04429e56c03b
Avinash Sharma <aviator1994@gmail.com>
2017-06-12 03:14:10 -0700

    Finished Q2 and Q4, still need to figure out Q3 and Q5

--- commands.sql
+++ commands.sql
@@ -14,41 +14,63 @@ where Day = '2016-12-30';
 
 -- 2015 - 2016 Increases
 select COUNT(*) as NumIncreases
-from (
-		select IF(SixClose > FifClose,"Increase", "Decrease") as IncDec
-		from (
-				select Ticker, AP1.Close as FifClose
-				from AdjustedPrices AP1
-				where Day = '2015-12-30'
-				group by Ticker
-	  		  ) Fifteen NATURAL JOIN
-	  		  (
-				select Ticker, AP2.Close as SixClose
-				from AdjustedPrices AP2
-				where Day = '2016-12-30'
-				group by Ticker
-			  ) Sixteen
-	  ) Changes
+from (select IF(SixClose > FifClose,"Increase", "Decrease") as IncDec
+from (select Ticker, AP1.Close as FifClose
+from AdjustedPrices AP1
+where Day = '2015-12-31'
+group by Ticker
+) Fifteen NATURAL JOIN
+(select Ticker, AP2.Close as SixClose
+from AdjustedPrices AP2
+where Day = '2016-12-30'
+group by Ticker
+) Sixteen
+) Changes
 where IncDec = "Increase";
 
 -- 2015 - 2016 Decreases
 select COUNT(*) as NumDecreases
-from (
-		select IF(SixClose < FifClose,"Increase", "Decrease") as IncDec
-		from (
-				select Ticker, AP1.Close as FifClose
-				from AdjustedPrices AP1
-				where Day = '2015-12-30'
-				group by Ticker
-	  		  ) Fifteen NATURAL JOIN
-	  		  (
-				select Ticker, AP2.Close as SixClose
-				from AdjustedPrices AP2
-				where Day = '2016-12-30'
-				group by Ticker
-			  ) Sixteen
-	  ) Changes
-where IncDec = "Increase";
+from (select IF(SixClose < FifClose,"Decrease", "Increase") as IncDec
+from (select Ticker, AP1.Close as FifClose
+from AdjustedPrices AP1
+where Day = '2015-12-31'
+group by Ticker
+) Fifteen NATURAL JOIN
+(select Ticker, AP2.Close as SixClose
+from AdjustedPrices AP2
+where Day = '2016-12-30'
+group by Ticker
+) Sixteen
+) Changes
+where IncDec = "Decrease";
 
--- Q2
+-- Q2 - Report top 10 stocks most heavily traded in 2016.
+select distinct S.Name, AP.Ticker
+from AdjustedPrices AP, Securities S
+where AP.Ticker = S.Ticker 
+and YEAR(AP.Day) = 2016
+order by AP.Volume desc
+limit 10;
+
+-- Q3 - For each year, report the top 5 highest performing stocks in terms
+-- of the absolute price increase and top 5 higheest performing stocks in
+-- terms of relative price increase.
+
+
+
+
+
+-- Q4 - Select 10 stocks to watch in 2017 based on stock market performance
+-- in 2016. Get top 10 highest average closes of distinct stocks in 2016.
+select Averages.Name, Averages.Ticker, Averages.Avg
+from (select S.Name, AP.Ticker, AVG(AP.Close) as Avg
+from AdjustedPrices AP, Securities S
+where AP.Ticker = S.Ticker
+and YEAR(AP.Day) = 2016
+group by AP.Ticker) Averages
+order by Averages.Avg desc
+limit 10;
+
+
+-- Q5 - 
 
