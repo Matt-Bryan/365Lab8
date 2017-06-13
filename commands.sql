5c87c8e6c10a8beb2bd2b952ebd541b9a4d2e42b
Matt-Bryan <mbryan_379@yahoo.com>
2017-06-12 21:27:00 -0700

    Did query 3 and started query 4 of Individual Queries

--- commands.sql
+++ commands.sql
@@ -262,3 +262,22 @@ group by S.Sector) LastAvgs
 where FirstAvgs.Sector = LastAvgs.Sector) Percentages;
 
 
+-- Queries for individual securities
+
+-- Q3
+select MONTH(Day) as Month, AVG(Close) as AvgClose, MAX(High) as HighestPrice, MIN(Low) as LowestPrice, AVG(Volume) as AvgVolume
+from AdjustedPrices AP
+where YEAR(Day) = 2016
+and Ticker = 'CB'
+group by MONTH(Day);
+
+-- Q4
+select Month
+from (
+		select MONTH(Day) as Month, SOMETHING HERE
+		from AdjustedPrices AP
+		where Ticker = 'CB'
+		group by MONTH(Day)
+	  ) MonthPerformances
+group by Month
+having SOMETHING HERE = MAX(SOMETHING HERE);
